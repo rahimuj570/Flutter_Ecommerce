f6560a0dca223630ca8d909476e9cbe755545003
rahimuj570 <rahimujjaman2002@gmail.com>
2025-12-13 16:50:20 +0600

    splash screen ui complete

--- lib/app/app_themes.dart
+++ lib/app/app_themes.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/app/app_colors.dart';
 
 class AppThemes {
   static ThemeData get light {
-    return ThemeData(brightness: Brightness.light);
+    return ThemeData(
+      brightness: Brightness.light,
+      primaryColor: AppColors.themeColor,
+    );
   }
 
   static ThemeData get dark {
-    return ThemeData(brightness: Brightness.dark);
+    return ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: AppColors.themeColor,
+    );
   }
 }
--- lib/features/auth/presentations/screens/splash_screen.dart
+++ lib/features/auth/presentations/screens/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/app/app_assets_path.dart';
 import 'package:flutter_ecommerce/app/extensions/localization_extension.dart';
 import 'package:flutter_ecommerce/app/state_management/language_provider.dart';
 import 'package:flutter_ecommerce/app/state_management/theme_provider.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -18,42 +20,32 @@ class _SplashScreenState extends State<SplashScreen> {
     ThemeProvider themeProvider = context.watch<ThemeProvider>();
     bool isDark = themeProvider.getThemeMode.name == 'dark';
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(context.localization.hello),
-            Text(context.localization.changeLaguage),
-
-            DropdownMenu(
-              onSelected: (value) => context
-                  .read<LanguageProvider>()
-                  .changeLocale(Locale(value.toString())),
-              initialSelection: context
-                  .read<LanguageProvider>()
-                  .getLocal
-                  .languageCode,
-              dropdownMenuEntries: [
-                DropdownMenuEntry(enabled: true, value: 'en', label: 'English'),
-                DropdownMenuEntry(value: 'bn', label: 'Bengali'),
+      body: Stack(
+        children: [
+          Center(child: SvgPicture.asset(AppAssetsPath.logo)),
+          Positioned(
+            bottom: 30,
+            right: 0,
+            left: 0,
+            child: Column(
+              children: [
+                CircularProgressIndicator(),
+                SizedBox(height: 30),
+                Text("Version 1.0"),
               ],
             ),
-            Switch(
-              value: isDark,
-              onChanged: (value) {
-                if (value) {
-                  themeProvider.changeThemeMode(ThemeMode.dark);
-                } else {
-                  themeProvider.changeThemeMode(ThemeMode.light);
-                }
-                isDark = value;
-                print(value);
-                // setState(() {});
-              },
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       SvgPicture.asset(AppAssetsPath.logo),
+      //       CircularProgressIndicator(),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
